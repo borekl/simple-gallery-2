9c5000bcba73c762a6d8aafedd1f91b4d841fa5d
Borek Lupomesky <borek@lupomesky.cz>
2018-12-04 13:54:52 +0100

    Add very basic script that generates the galleries/sets
    This simple script ties everything together in a very simple way.  No
    command-line arguments are supported, no configuration etc.

--- gallery.pl
+++ gallery.pl
@@ -4,7 +4,27 @@ use v5.10;
 use utf8;
 use FindBin qw($Bin);
 use lib "$Bin/lib";
+use Text::Pluralize;
 
 use Gallery;
+use GallerySet;
 
-Gallery->new()->write_index('index.json');
+if(-r 'info.json') {
+  Gallery->new()->write_index('index.json');
+} else {
+  say "Gallery set mode";
+  my $gs = GallerySet->new();
+
+  say
+    pluralize('%d galler{ies|y|ies} found, ', $gs->count()),
+    pluralize('%d need{|s|} updating', scalar($gs->update_needed()));
+
+  print 'Writing gallery set index ... ';
+  $gs->write_index();
+  say 'done';
+
+  $gs->update_galleries(sub {
+    my $g = shift;
+    say $g->dir(), ' updated';
+  });
+}
